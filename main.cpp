26de910b8fb8dc4b0856b12ff1c03f9631083dbe
chioujianooi <chioujian.ooi@gmail.com>
2026-03-25 17:57:30 +0100

    simple client

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include "socket.h"
+#include <iostream>
+#include <string>
 
 int main(){
+        ClientSocket client;
+    client.create();
+    std::string ip="192.168.56.1";
+    client.customConnect(8080, ip.c_str(), ip.length());
+    client.sendData("Hello from client!", 18);
+    char buffer[1024];
+    client.receiveData(buffer, sizeof(buffer));
+    std::cout<< "Received: " << buffer << std::endl;
+
+    
+
     return 0;
 }
